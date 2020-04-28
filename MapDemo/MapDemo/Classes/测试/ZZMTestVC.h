a29940bc83581930d753e5cbee0209536ade6989
Your Name <you@example.com>
2020-04-28 15:42:45 +0800

    测试更新

---
+++ MapDemo/MapDemo/Classes/测试/ZZMTestVC.h
@@ -0,0 +1,17 @@
+//
+//  ZZMTestVC.h
+//  MapDemo
+//
+//  Created by zhouzhongmao on 2020/4/28.
+//  Copyright © 2020 zhouzhongmao. All rights reserved.
+//
+
+#import "BaseVC.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZZMTestVC : BaseVC
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MapDemo/MapDemo/Classes/测试/ZZMTestVC.m
@@ -0,0 +1,32 @@
+//
+//  ZZMTestVC.m
+//  MapDemo
+//
+//  Created by zhouzhongmao on 2020/4/28.
+//  Copyright © 2020 zhouzhongmao. All rights reserved.
+//
+
+#import "ZZMTestVC.h"
+
+@interface ZZMTestVC ()
+
+@end
+
+@implementation ZZMTestVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
